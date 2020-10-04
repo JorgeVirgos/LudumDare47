41cbc14469ed0c4695d4f515ab8d09499a619822
tenedor <queteimportaloquedigo16@gmail.com>
2020-10-04 20:32:28 +0200

    spawner

--- Assets/Scripts/SpawnEnemiesInRoom.cs
+++ Assets/Scripts/SpawnEnemiesInRoom.cs
@@ -1,6 +1,7 @@
-﻿using System;
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Threading;
 using UnityEngine;
 
 public class SpawnEnemiesInRoom : MonoBehaviour
@@ -9,10 +10,11 @@ public class SpawnEnemiesInRoom : MonoBehaviour
     public GameObject current_level = null;
     public GameObject PistolEnemy, RifleEnemy, ShotgunEnemy;
     public int num = 3;
+   
     // Start is called before the first frame update
     void Start()
     {
-        
+        SpawnIntoRoom();
     }
 
     // Update is called once per frame
@@ -20,37 +22,43 @@ public class SpawnEnemiesInRoom : MonoBehaviour
     {
         
     }
-    public void FindObjectwithTag(string _tag)
-    {
-        actors.Clear();
-        Transform parent = transform;
-        GetChildObject(parent, _tag);
-    }
    
+
+
+    public Vector3 getRandomSpawnPoint() {
+        GameObject[] spawnpoints = GameObject.FindGameObjectsWithTag("spawner");
+        return spawnpoints[Random.Range(0, spawnpoints.Length)].transform.position;
+
+    }
+    void Spawn(GameObject e1, GameObject e2)
+    {
+        for (int i = 0; i < num; i++) {
+            GameObject enemy=new GameObject();
+            switch (Random.Range(1, 3))
+            {
+                case 1:
+                    enemy = e1;
+                    break;
+                case 2:
+                    enemy = e2;
+                    break;
+
+            }
+            Instantiate(enemy, getRandomSpawnPoint(), Quaternion.identity); 
+        }
+
+    }
+
     void SpawnIntoRoom() {
         if (current_level.name != "emptyRoom") {
             GameObject[] spawnpoints;
-            spawnpoints=GameObject.FindGameObjectsWithTag("spawner");
-            if (current_level.transform.FindChild("suelo").tag == "shortRange") {
-                for (int i = 0; i < spawnpoints.Length; i++) {
-                    GameObject enemyToInst;
-                    switch (Random.Range(1, 3))
-                    {
-                        case 1:
-                            enemyToInst = PistolEnemy;
-                         break;
-                        case 2:
-                            enemyToInst = ShotgunEnemy;
-                        break;
-
-                    }  
-                    Instantiate(enemyToInst[UnityEngine.Random.Range(0, enemy.Length - 1)], transform.position, Quaternion.identity);
-                }
-    
+            if (current_level.transform.FindChild("suelo").tag == "shortRangeRoom")
+            {
+                Spawn(PistolEnemy, ShotgunEnemy);
             }
             else
             {
-
+                Spawn(PistolEnemy, RifleEnemy);
             }
            
          }

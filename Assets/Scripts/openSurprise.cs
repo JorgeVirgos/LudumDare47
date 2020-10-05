601f9182315d0f0f1377b2003260ee8396bd76f2
Aitor-M <aitor96mar@protonmail.com>
2020-10-05 23:17:10 +0200

    Added sorpresa and exit prefab

--- Assets/Scripts/openSurprise.cs
+++ Assets/Scripts/openSurprise.cs
@@ -4,24 +4,24 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 public class openSurprise : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+  // Start is called before the first frame update
+  void Start()
+  {
+
+  }
+
+  // Update is called once per frame
+  void OnCollisionEnter(Collision collision)
+  {
+    if (collision.gameObject.tag == "Player")
     {
+      GameObject mc = GameObject.FindGameObjectWithTag("MainCamera");
+      mc.SetActive(false);
+      // Only specifying the sceneName or sceneBuildIndex will load the Scene with the Single mode
+      SceneManager.LoadScene("Sorpresa");
 
     }
-
-    // Update is called once per frame
-    void OnCollisionEnter(Collision collision)
-    {
-        if (collision.gameObject.tag == "Projectile") return;
-        if (collision.gameObject.tag == "Player")
-        {
-
-            // Only specifying the sceneName or sceneBuildIndex will load the Scene with the Single mode
-            SceneManager.LoadScene("Sorpresa");
-           
-        }
-    }
+  }
 }
-       
+
 

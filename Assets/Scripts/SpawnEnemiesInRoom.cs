3a7c1f7532184434279586bbe3bed772b7ac9cdb
tenedor <queteimportaloquedigo16@gmail.com>
2020-10-05 13:59:20 +0200

    nav mesh in boys

--- Assets/Scripts/SpawnEnemiesInRoom.cs
+++ Assets/Scripts/SpawnEnemiesInRoom.cs
@@ -12,13 +12,18 @@ public class SpawnEnemiesInRoom : MonoBehaviour
     public GameObject current_level = null;
     public GameObject PistolEnemy, RifleEnemy, ShotgunEnemy;
     public int num = 3;
-     List<GameObject>  spawnpoints=new List<GameObject>();
-
+    List<GameObject>  spawnpoints=new List<GameObject>();
+    List<GameObject> boxes = new List<GameObject>();
     // Start is called before the first frame update
     void Start()
     {
       
         SpawnIntoRoom();
+        if (current_level.tag == "RandomRoom")
+        {
+            removeBoxes();
+        }
+
     }
 
     // Update is called once per frame
@@ -27,23 +32,38 @@ public class SpawnEnemiesInRoom : MonoBehaviour
         
     }
 
-    public void GetSpawnPoints(GameObject c) {
+    public void GetSpawnPointsAndBoxes(GameObject c) {
         Transform t = c.transform;
          foreach (Transform tr in t)
         {
-            
-            if (tr.tag == "spawner")
+             if (tr.tag == "spawner")
             {
                 spawnpoints.Add(tr.gameObject);
             }
+            if (tr.tag == "removableBox")
+            {
+                boxes.Add(tr.gameObject);
+            }
         }
        
+       
+
+    }
+    public void removeBoxes() {
+       
+        for (int i = 0; i < boxes.Count; i++)
+        {
+            int chance = UnityEngine.Random.Range(2, 5);
+            if (UnityEngine.Random.Range(0, chance) == 0)
+            {
+                Destroy(boxes[i]);
+            }
+        }
+        
     }
 
     public Vector3 getRandomSpawnPoint() {
-      
         return spawnpoints[UnityEngine.Random.Range(0,spawnpoints.Count)].transform.position;
-
     }
     void Spawn(GameObject e1, GameObject e2)
     {
@@ -60,16 +80,14 @@ public class SpawnEnemiesInRoom : MonoBehaviour
                     break;
 
             }
-            
-               
-            
+       
         }
 
     }
 
     void SpawnIntoRoom() {
         if (current_level.name != "emptyRoom") {
-            GetSpawnPoints(current_level.transform.FindChild("suelo").gameObject);
+            GetSpawnPointsAndBoxes(current_level.transform.FindChild("suelo").gameObject);
             if (current_level.transform.FindChild("suelo").tag == "shortRangeRoom")
             {
                 Spawn(PistolEnemy, ShotgunEnemy);

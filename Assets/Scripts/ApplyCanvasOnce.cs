7412669af4e599a204040801243b2ab874541848
Aitor-M <aitor96mar@protonmail.com>
2020-10-05 21:13:55 +0200

    Showing canvas on Start

---
+++ Assets/Scripts/ApplyCanvasOnce.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ApplyCanvasOnce : MonoBehaviour
+{
+  PlayerController pc;
+  Canvas parentCanvas;
+
+  void Start()
+  {
+    Cursor.lockState = CursorLockMode.None;
+    Cursor.visible = true;
+
+    GameObject player = GameObject.FindGameObjectWithTag("Player");
+    pc = player.GetComponent<PlayerController>();
+    pc.enabled = false;
+    parentCanvas = GetComponentInParent<Canvas>();
+    parentCanvas.sortingOrder = 10;
+  }
+
+  public void EnablePlayer()
+  {
+    pc.enabled = true;
+    Destroy(this.gameObject);
+    parentCanvas.sortingOrder = 0;
+    Cursor.lockState = CursorLockMode.Locked;
+    Cursor.visible = false;
+  }
+}

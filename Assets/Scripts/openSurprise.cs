b7879d08cd43fce9d347a1d4faa79406bd9c194c
tenedor <queteimportaloquedigo16@gmail.com>
2020-10-05 22:58:24 +0200

    exitRoom

---
+++ Assets/Scripts/openSurprise.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class openSurprise : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Projectile") return;
+        if (collision.gameObject.tag == "Player")
+        {
+
+            // Only specifying the sceneName or sceneBuildIndex will load the Scene with the Single mode
+            SceneManager.LoadScene("Sorpresa");
+           
+        }
+    }
+}
+       
+
